d94eee8789d1d3f18e706301a1e9190679cc2793
andphan <amannamedphan@gmail.com>
2015-04-20 18:32:23 -0700

    teest

---
+++ src/actv/ccs/model/ui/Main.java
@@ -0,0 +1,12 @@
+package actv.ccs.model.ui;
+
+import actv.ccs.sageTest.MyGame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MyGame().start();
+	}
+
+}
